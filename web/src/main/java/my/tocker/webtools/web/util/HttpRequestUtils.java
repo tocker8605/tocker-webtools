5f255ff141a2779a4c93295cabb0c328d424f296
tocker <tocker8605@gmail.com>
2018-07-15 20:40:31 +0900

    request util fix

--- web/src/main/java/my/tocker/webtools/web/util/HttpRequestUtils.java
+++ web/src/main/java/my/tocker/webtools/web/util/HttpRequestUtils.java
@@ -1,15 +1,27 @@
 package my.tocker.webtools.web.util;
 
-import java.util.Arrays;
-import java.util.Map;
-import java.util.Objects;
-import java.util.stream.Collectors;
-
 import com.google.common.base.Strings;
 import com.google.common.collect.Maps;
-import javafx.util.Pair;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class HttpRequestUtils {
+
+    @Setter
+    @Getter
+    @AllArgsConstructor
+    @NoArgsConstructor
+    private static class DataEntry {
+        private String key;
+        private String value;
+    }
 
     public static Map<String, String> parseQueryString(String queryString) {
         return parseValues(queryString, "&");
@@ -27,25 +39,27 @@ public class HttpRequestUtils {
         String[] tokens = values.split(separator);
         return Arrays.stream(tokens)
                 .map(t -> getKeyValue(t, "="))
-                .filter(Objects::nonNull)
-                .collect(Collectors.toMap(Pair::getKey, Pair::getValue));
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toMap(DataEntry::getKey, DataEntry::getValue));
     }
 
-    static Pair<String, String> getKeyValue(String keyValue, String regex) {
-        if (Strings.isNullOrEmpty(keyValue)) {
-            return null;
+    private static Optional<DataEntry> getKeyValue(String stringToken, String regex) {
+        String kv = stringToken.trim();
+        if (Strings.isNullOrEmpty(kv)) {
+            return Optional.empty();
         }
 
-        String[] tokens = keyValue.split(regex);
+        String[] tokens = kv.split(regex);
         if (tokens.length != 2) {
-            return null;
+            return Optional.empty();
         }
 
-        return new Pair<>(tokens[0], tokens[1]);
+        return Optional.of(new DataEntry(tokens[0].trim(), tokens[1].trim()));
     }
 
-    public static Pair parseHeader(String header) {
-        return getKeyValue(header, ": ");
+    public static String parseHeader(String header) {
+        return getKeyValue(header, ":").orElse(new DataEntry()).getValue();
     }
 
 
